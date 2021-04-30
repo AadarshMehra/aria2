1199acd77e2542c6a55448a2cd5f7cfb6828cd5d
AadarshMehra <61242909+AadarshMehra@users.noreply.github.com>
2021-04-30 09:33:42 +0530

    Update dwld.py

--- dwld.py
+++ dwld.py
@@ -6,7 +6,7 @@ url = r'https://trackerslist.com/all_aria2.txt'
 fileOpen = request.urlopen(url)
 info = fileOpen.read()
 
-static="dir=/content/drive/Shareddrives/Movies/Movies\nalways-resume=true\nmax-concurrent-downloads=50\ncheck-integrity=true\nmax-connection-per-server=16\nuser-agent=qBittorrent/4.3.3\npeer-agent=qbittorrent/4.3.3\npeer-id-prefix=-qB4330-\ndisk-cache=0\nfile-allocation=none\nseed-ratio=0.0\nbt-tracker="
+static="dir=/content/drive/Shareddrives/My Shared Drive/Movies\nalways-resume=true\nmax-concurrent-downloads=50\ncontinue=true\ncheck-integrity=true\nmax-connection-per-server=16\nuser-agent=qBittorrent/4.3.3\npeer-agent=qbittorrent/4.3.3\npeer-id-prefix=-qB4330-\ndisk-cache=0\nfile-allocation=none\nseed-ratio=0.0\nbt-tracker="
 info=info.decode("utf-8")
 info=(static+info)
 f=open(r"/home/.aria2/aria2.conf", "x")
